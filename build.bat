cee1577e43d5c05a5e14f630543083a8b70e89ab
Christopher Field <cfield2@gmail.com>
2018-03-12 18:51:03 -0400

    Add renaming DLLs to build

--- build.bat
+++ build.bat
@@ -6,3 +6,17 @@ cmake -G "Visual Studio 15 2017 Win64" .. -DBUILD_SHARED_LIBS=ON
 popd
 cmake --build bin32 --config Release
 cmake --build bin64 --config Release
+pushd bin32
+pushd src
+pushd Release
+ren libssh2.dll labssh2.dll
+popd
+popd
+popd
+pushd bin64
+pushd src
+pushd Release
+ren libssh2.dll labssh2-x64.dll
+popd
+popd
+popd
