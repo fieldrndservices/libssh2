7c2ded3c31be717ed33d0413dc4cdef087ef608a
Christopher Field <cfield2@gmail.com>
2018-03-19 15:56:11 -0400

    Fix output directory names

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
-if not exist "build32\" mkdir bin32 
-pushd bin32
-cmake -G"Visual Studio 15 2017" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLE=OFF -DBUILD_TESTING=OFF -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR="C:\Program Files (x86)\OpenSSL\" .. 
+if not exist "build32\" mkdir build32 
+pushd build32
+cmake -G"Visual Studio 15 2017" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLE=OFF -DBUILD_TESTING=OFF -DBUILD_DOCS=OFF -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR="C:\Program Files (x86)\OpenSSL\" .. 
 popd
-if not exist "build64\" mkdir bin64 
-pushd bin64
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLE=OFF -DBUILD_TESTING=OFF -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL" .. 
+if not exist "build64\" mkdir build64 
+pushd build64
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLE=OFF -DBUILD_TESTING=OFF -DBUILD_DOCS=OFF -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL" .. 
 popd
-cmake --build bin32 --config Release
-cmake --build bin64 --config Release
-pushd bin64 & pushd src & pushd Release
+cmake --build build32 --config Release
+cmake --build build64 --config Release
+pushd build64 & pushd src & pushd Release
 if exist "labssh2-x64.dll" del "labssh2-x64.dll"
 ren labssh2.dll labssh2-x64.dll
 popd & popd & popd
