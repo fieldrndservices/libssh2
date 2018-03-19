267a77e4641dea961f571230b8367931630394b5
Christopher Field <cfield2@gmail.com>
2018-03-19 16:24:43 -0400

    Fix path to OpenSSL library

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 if not exist "build32\" mkdir build32 
 pushd build32
-cmake -G"Visual Studio 15 2017" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DBUILD_DOCS=OFF -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR="C:\Program Files (x86)\OpenSSL\" .. 
+cmake -G"Visual Studio 15 2017" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DBUILD_DOCS=OFF -DCRYPTO_BACKEND=OpenSSL -DOPENSSL_ROOT_DIR="C:\Program Files (x86)\OpenSSL" .. 
 popd
 if not exist "build64\" mkdir build64 
 pushd build64
